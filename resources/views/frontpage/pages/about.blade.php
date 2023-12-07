2dcd54f0a9c5ef0d800c1bc4e033f5614e8e67e7
M Syahrul Maajid S <mohsyahrul>
2023-12-07 13:02:28 +0700

    Reservasi Barbershop

---
+++ resources/views/frontpage/pages/about.blade.php
@@ -0,0 +1,7 @@
+@extends('frontpage.layouts.main')
+@section('container')
+<div class="mt-4 mx-5 d-block justify-content-center">
+    <h1>Project Tugas Akhir SBD 2023</h1>
+    <h2>by@Kelompok 22</h2>
+</div>
+@endsection
